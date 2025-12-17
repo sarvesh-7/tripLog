36dccd3d9a1e30639d12b81df5e46acbdf039d35
sarvesh-7 <sarveshgawade42@gmail.com>
2025-12-17 17:54:29 +0530

    made homepage responsive

--- app/page.tsx
+++ app/page.tsx
@@ -6,32 +6,32 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <header className="m-auto flex justify-center gap-9 mt-10 w-[90%] items-center">
-        <div className="relative w-[25rem] h-[25rem] overflow-hidden rounded-xl">
+      <header className="m-auto flex flex-col lg:flex-row justify-center gap-9 mt-10 w-[90%] items-center">
+        <div className="relative w-[18rem] h-[18rem] sm:w-[25rem] sm:h-[25rem] overflow-hidden rounded-xl">
           <Image src={heroImage} alt="hero image" fill />
         </div>
-        <div className="flex flex-col gap-6 text-[1.5rem]">
-          <h1 className="uppercase text-6xl font-bold bg-linear-to-r from-[#f9572a] to-[#ffc905] bg-clip-text text-transparent">
+        <div className="flex flex-col gap-6 sm:text-[1.5rem] ">
+          <h1 className="uppercase text-3xl lg:text-6xl text-center lg:text-left font-bold bg-linear-to-r from-[#f9572a] to-[#ffc905] bg-clip-text text-transparent">
             <p>Trip log for</p>
             <p>travel enthusiasts</p>
           </h1>
           <div>
             Capture and share travel memories from around the globe
           </div>
-          <div className="flex gap-6 items-center">
-            <Link href="/community" className="text-[#ffc905] hover:text-[#fadf7d]">Join the community</Link>
+          <div className="flex gap-6 items-center justify-center lg:justify-start">
+            <Link href="/community" className="text-center text-[#ffc905] hover:text-[#fadf7d]">Join the community</Link>
             <Link
               href="/trips"
-              className="hover:cursor-pointer bg-linear-to-r from-[#f9572a] to-[#d2a915] rounded-md px-2 py-2 font-bold">
+              className="text-center hover:cursor-pointer bg-linear-to-r from-[#f9572a] to-[#d2a915] rounded-md px-2 py-2 font-bold">
               Explore Memories
             </Link>
           </div>
         </div>
       </header>
-      <main className="flex m-auto w-[50%] justify-center mt-10">
+      <main className="flex m-auto md:w-[70%] lg:w-[50%] justify-center mt-10">
         <div className="flex flex-col p-5 text-center gap-20">
           <div className="flex flex-col gap-12">
-            <p className="text-6xl font-bold">How it works</p>
+            <p className="text-4xl sm:text-6xl font-bold">How it works</p>
             <p>
               Trip Logs is a tiny journal for travelers.
               Add a short entry with a photo, a few highlights, where it happened, and the date
@@ -43,7 +43,7 @@ export default function Home() {
             </p>
           </div>
           <div className="flex flex-col gap-12">
-            <p className="text-6xl font-bold">Why Trip Logs</p>
+            <p className="text-4xl sm:text-6xl font-bold">Why Trip Logs</p>
             <p>
               Trip Logs is a tiny journal for travelers.
               Add a short entry with a photo, a few highlights, where it happened, and the date
--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import AppLogo from "@/assets/AppLogo.png";
@@ -7,6 +7,8 @@ import { usePathname } from 'next/navigation';
 import NavLink from './NavLink';
 
 function MainHeader() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <header className="w-full bg-linear-to-r from-[#6a5247] to-[#8f3a09] text-white shadow-md">
@@ -17,7 +19,7 @@ function MainHeader() {
                         NEXTLEVEL TRIP
                     </Link>
                 </div>
-                <ul className='flex gap-6 font-bold'>
+                <ul className='hidden md:flex gap-6 font-bold'>
                     <li>
                         <NavLink href="/trips">
                             Browse Trips
@@ -29,7 +31,33 @@ function MainHeader() {
                         </NavLink>
                     </li>
                 </ul>
+                <button
+                    className='md:hidden flex flex-col gap-1 bg-[#282c34]'
+                    aria-label='Toggle Menu'
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    <span className='h-0.5 w-6 bg-white'></span>
+                    <span className='h-0.5 w-6 bg-white'></span>
+                    <span className='h-0.5 w-6 bg-white'></span>
+                </button>
             </nav>
+            {/* Mobile Menu */}
+            {menuOpen && (
+                <div className='md:hidden px-6 pb-4'>
+                    <ul className='flex flex-col gap-3 font-bold'>
+                        <li>
+                            <NavLink href="/trips">
+                                Browse Trips
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink href="/community">
+                                Trips Community
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            )}
         </header>
     );
 }
